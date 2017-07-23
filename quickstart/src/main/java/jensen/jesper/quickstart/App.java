ff9f9fd27d3e0be9877228ebe75eaa55cf5032f0
JesperJensen <JesperJensen@DESKTOP-IF5468V>
2017-07-23 09:54:34 +0200

    rre

---
+++ quickstart/src/main/java/jensen/jesper/quickstart/App.java
@@ -0,0 +1,12 @@
+package jensen.jesper.quickstart;
+
+import com.google.gson.Gson;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!") );
+    }
+}
